5b216c49f3b531f6e5310a175ce8be02bcc62071
Munish pthnia <manishpathania092@gmail.com>
2022-10-06 13:36:54 +0530

    uplaoding the clone of notes app

---
+++ src/notes.js
@@ -0,0 +1,30 @@
+const notes = [
+    {
+      id: 1,
+      title: "Arays",
+      content:"An array is a collection of similar types of data. For example, if we want to store the names of 100 people then we can create an array of the string type that can store 100 names"
+    },
+    {
+        id: 2,
+        title: "Arays",
+        content:"An array is a collection of similar types of data. For example, if we want to store the names of 100 people then we can create an array of the string type that can store 100 names"
+      },
+      {
+        id: 3,
+        title: "Arays",
+        content:"An array is a collection of similar types of data. For example, if we want to store the names of 100 people then we can create an array of the string type that can store 100 names"
+      },
+      {
+        id: 4,
+        title: "Arays",
+        content:"An array is a collection of similar types of data. For example, if we want to store the names of 100 people then we can create an array of the string type that can store 100 names"
+      },
+      {
+        id: 4,
+        title: "Arays",
+        content:"An array is a collection of similar types of data. For example, if we want to store the names of 100 people then we can create an array of the string type that can store 100 names"
+      }
+   
+  ];
+  
+  export default notes;
